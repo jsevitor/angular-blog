9bf970449fb910b9af9ec5ad83613fa008b1c7db
José Vitor Oliveira Reis <vitorjseo@gmail.com>
2024-02-21 16:03:46 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png",
+        "title": "Lorem ipsum dolor sit amet consectetur",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum cupiditate incidunt veritatis maxime quo, tempora eius laboriosam. Aliquam ad non reprehenderit possimus! Delectus laboriosam facilis sed inventore suscipit autem eos aliquid voluptate excepturi modi illo animi asperiores eligendi ducimus, voluptas consequuntur laudantium numquam corrupti esse minus eius provident quibusdam doloremque. Odio numquam quos dolores esse? Rerum mollitia ab fuga tenetur alias, nihil perspiciatis laudantium iure. Sunt illo amet quis doloremque architecto! Hic error nesciunt unde in debitis alias quidem molestias? Illo aliquid, omnis hic amet obcaecati quibusdam nemo magnam maiores dignissimos minima iste beatae eaque aperiam officia quaerat similique quisquam?"
+    },
+    {
+        "id": "2",
+        "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png",
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga expedita animi dolorum.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum cupiditate incidunt veritatis maxime quo, tempora eius laboriosam. Aliquam ad non reprehenderit possimus! Delectus laboriosam facilis sed inventore suscipit autem eos aliquid voluptate excepturi modi illo animi asperiores eligendi ducimus, voluptas consequuntur laudantium numquam corrupti esse minus eius provident quibusdam doloremque. Odio numquam quos dolores esse? Rerum mollitia ab fuga tenetur alias, nihil perspiciatis laudantium iure. Sunt illo amet quis doloremque architecto! Hic error nesciunt unde in debitis alias quidem molestias? Illo aliquid, omnis hic amet obcaecati quibusdam nemo magnam maiores dignissimos minima iste beatae eaque aperiam officia quaerat similique quisquam?"
+    },
+    {
+        "id": "3",
+        "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png",
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti minima eos delectus sit laboriosam, asperiores qui.",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum cupiditate incidunt veritatis maxime quo, tempora eius laboriosam. Aliquam ad non reprehenderit possimus! Delectus laboriosam facilis sed inventore suscipit autem eos aliquid voluptate excepturi modi illo animi asperiores eligendi ducimus, voluptas consequuntur laudantium numquam corrupti esse minus eius provident quibusdam doloremque. Odio numquam quos dolores esse? Rerum mollitia ab fuga tenetur alias, nihil perspiciatis laudantium iure. Sunt illo amet quis doloremque architecto! Hic error nesciunt unde in debitis alias quidem molestias? Illo aliquid, omnis hic amet obcaecati quibusdam nemo magnam maiores dignissimos minima iste beatae eaque aperiam officia quaerat similique quisquam?"
+    },
+    {
+        "id": "4",
+        "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png",
+        "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, recusandae?",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum cupiditate incidunt veritatis maxime quo, tempora eius laboriosam. Aliquam ad non reprehenderit possimus! Delectus laboriosam facilis sed inventore suscipit autem eos aliquid voluptate excepturi modi illo animi asperiores eligendi ducimus, voluptas consequuntur laudantium numquam corrupti esse minus eius provident quibusdam doloremque. Odio numquam quos dolores esse? Rerum mollitia ab fuga tenetur alias, nihil perspiciatis laudantium iure. Sunt illo amet quis doloremque architecto! Hic error nesciunt unde in debitis alias quidem molestias? Illo aliquid, omnis hic amet obcaecati quibusdam nemo magnam maiores dignissimos minima iste beatae eaque aperiam officia quaerat similique quisquam?"
+    }
+]
